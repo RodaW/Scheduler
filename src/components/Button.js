a2ddfb5b235562a94501c0554669d0606bfffa59
Roda W <93175113+RodaW@users.noreply.github.com>
2022-01-21 14:58:08 -0500

    added button component & refractored

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
+export default function Button({ danger, confirm, onClick, disabled, children }) {
+
+   return (
+      <button
+         className={classNames("button",{ "button--confirm": confirm }, { "button--danger": danger })}
+         disabled={disabled}
+         onClick={onClick}>
+         {children}
+      </button>);
 }
